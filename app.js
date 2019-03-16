8000f848b8b48906f9f18f7546fb042c2922324f
myeonghyeon Jeong <myeonghyeonjeong@myeonghyeonui-MacBookPro.local>
2019-03-16 13:56:43 +0900

    ADD routes, controllers

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const userRouter = require("./routes/users");
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+// Routes
+app.use("/users", userRouter);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
+
+console.log(`Server listening at ${PORT}`);
