0d712f9bd875a9b6e1169a41a13eb1f59454b0b2
myeonghyeon Jeong <myeonghyeonjeong@myeonghyeonui-MacBookPro.local>
2019-03-16 14:56:10 +0900

    mongoDB connect / ADD signUp

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+        "mongodb+srv://jeongmyeonghyeon:1111@cluster0-ysiqz.mongodb.net/db?retryWrites=true"
+};
--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,8 @@ const { validateBody, schemas } = require("../helpers/routerHelpers");
 const UserController = require("../controllers/users");
 
 router
-  .route("/signup")
-  .post(validateBody(schemas.authSchema), UserController.signUp);
+    .route("/signup")
+    .post(validateBody(schemas.authSchema), UserController.signUp);
 router.route("/signin").post(UserController.signIn);
 router.route("/secret").post(UserController.secret);
 
