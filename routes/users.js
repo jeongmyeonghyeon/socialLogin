bce46146d43fa2ef9f4f646254de5100ac45a3dd
myeonghyeon Jeong <myeonghyeonjeong@myeonghyeonui-MacBookPro.local>
2019-03-16 14:21:15 +0900

    ADD validation

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = require("express-promise-router")();
 
+const { validateBody, schemas } = require("../helpers/routerHelpers");
+
 const UserController = require("../controllers/users");
 
-router.route("/signup").post(UserController.signUp);
+router
+  .route("/signup")
+  .post(validateBody(schemas.authSchema), UserController.signUp);
 router.route("/signin").post(UserController.signIn);
 router.route("/secret").post(UserController.secret);
 
