cc69eef958fb396e8bf8841b739dd9ceebff6880
myeonghyeon Jeong <myeonghyeonjeong@myeonghyeonui-MacBookPro.local>
2019-03-16 15:21:06 +0900

    passport.js 작성

---
+++ passport.js
@@ -0,0 +1,30 @@
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const { ExtractJwt } = require("passport-jwt");
+const LocalStrategy = require("passport-local").Strategy;
+const { JWT_SECRET } = require("./config");
+const User = require("./models/user");
+
+passport.use(
+    new JwtStrategy(
+        {
+            jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+            _secretOrKey: JWT_SECRET
+        },
+        async (payload, done) => {
+            try {
+                // Find the user specified in token
+                const user = await User.findById(payload.sub);
+
+                // If user doesn't exists, handle it
+                if (!user) {
+                    return done(null, false);
+                }
+                // Otherwise, return the user
+                done(null, user);
+            } catch (error) {
+                done(error.false);
+            }
+        }
+    )
+);
